ce4b67bfe5acc5abdf5cd37b8dfff2fb87772ed3
tobiasdollhofer <tdollhofer96@googlemail.com>
2021-05-24 18:57:54 +0200

    implemented player controlls working listeners

--- src/main/java/de/tobiasdollhofer/codecast/ui/CodeCastPlayer.java
+++ src/main/java/de/tobiasdollhofer/codecast/ui/CodeCastPlayer.java
@@ -9,6 +9,7 @@ import com.intellij.ui.components.JBList;
 import de.tobiasdollhofer.codecast.player.data.Chapter;
 import de.tobiasdollhofer.codecast.player.data.Playlist;
 import de.tobiasdollhofer.codecast.player.service.PlaylistService;
+import de.tobiasdollhofer.codecast.player.util.PluginIcons;
 
 import javax.swing.*;
 
@@ -23,6 +24,15 @@ public class CodeCastPlayer {
     private JList playlistList;
     private JTextPane progressTime;
     private JProgressBar playerProgressBar;
+    private JButton playFirst;
+    private JButton playPrevious;
+    private JButton playPause;
+    private JButton playNext;
+    private JButton playLast;
+    private JSlider volumeSlider;
+    private JLabel volumeIcon;
+
+    private boolean playing = false;
 
     private Project project;
 
@@ -30,11 +40,67 @@ public class CodeCastPlayer {
         this.project = project;
         initList();
         initToolbarListener();
+        initPlayerControls();
+
+    }
+
+    private void initPlayerControls() {
+        initPlayerControlIcons();
         initPlayerControlListener();
     }
 
-    private void initPlayerControlListener() {
+    private void initPlayerControlIcons() {
+        playFirst.setIcon(PluginIcons.playFirst);
+        playPrevious.setIcon(PluginIcons.playPrevious);
+        playPause.setIcon(PluginIcons.play);
+        playNext.setIcon(PluginIcons.playNext);
+        playLast.setIcon(PluginIcons.playLast);
+        volumeIcon.setIcon(PluginIcons.volume);
+    }
 
+    private void initPlayerControlListener() {
+        playFirst.addActionListener(e -> playFirstClicked());
+        playPrevious.addActionListener(e -> playPreviousClicked());
+        playPause.addActionListener(e -> playPauseClicked());
+        playNext.addActionListener(e -> playNextClicked());
+        playLast.addActionListener(e -> playLastClicked());
+        volumeSlider.addChangeListener(e -> volumeSliderChange());
+    }
+
+    private void volumeSliderChange() {
+        System.out.println("New Volume: " + volumeSlider.getValue());
+        if(volumeSlider.getValue() == 0){
+            volumeIcon.setIcon(PluginIcons.volumeOff);
+        }else{
+            volumeIcon.setIcon(PluginIcons.volume);
+        }
+    }
+
+    private void playLastClicked() {
+        System.out.println("Play Last clicked!");
+    }
+
+    private void playNextClicked() {
+        System.out.println("Play Next clicked!");
+    }
+
+    private void playPauseClicked() {
+        System.out.println("Play Pause clicked!");
+        if(playing){
+            playPause.setIcon(PluginIcons.play);
+        }else{
+            playPause.setIcon(PluginIcons.pause);
+        }
+        playing = !playing;
+
+    }
+
+    private void playPreviousClicked() {
+        System.out.println("Play Previous clicked!");
+    }
+
+    private void playFirstClicked() {
+        System.out.println("Play First clicked!");
     }
 
     private void initToolbarListener() {
