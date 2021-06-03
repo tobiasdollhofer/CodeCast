5685c37e7f8dec38812a4610a712db9e334583cb
tobiasdollhofer <tdollhofer96@googlemail.com>
2021-06-03 11:38:46 +0200

    implemented function to get all files with codecast annotation in it

--- src/main/java/de/tobiasdollhofer/codecast/player/service/PlaylistServiceImpl.java
+++ src/main/java/de/tobiasdollhofer/codecast/player/service/PlaylistServiceImpl.java
@@ -1,10 +1,22 @@
 package de.tobiasdollhofer.codecast.player.service;
 
 import com.intellij.openapi.components.Service;
+import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.impl.java.stubs.index.JavaStubIndexKeys;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndexImpl;
 import de.tobiasdollhofer.codecast.player.data.Playlist;
 import de.tobiasdollhofer.codecast.player.util.FilePathUtil;
 import de.tobiasdollhofer.codecast.player.util.PlaylistLoader;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class PlaylistServiceImpl implements PlaylistService{
@@ -15,7 +27,6 @@ public class PlaylistServiceImpl implements PlaylistService{
     public PlaylistServiceImpl(Project project) {
         this.project = project;
         loadPlaylist();
-        System.out.println(playlist.toString());
     }
 
     @Override
@@ -26,6 +37,29 @@ public class PlaylistServiceImpl implements PlaylistService{
     @Override
     public void loadPlaylist() {
         playlist = PlaylistLoader.loadFromMetaFile(FilePathUtil.getCodeCastMetaPath(project));
+        loadPlaylistFromComments();
+    }
+
+    private void loadPlaylistFromComments() {
+        System.out.println("Load Playlist From Comments");
+
+
+        Collection<VirtualFile> files = FilenameIndex.getAllFilesByExt(project, "java", GlobalSearchScope.projectScope(project));
+        ArrayList<VirtualFile> codecastFiles = getAllCodecastFiles(files);
+
+    }
+
+    private ArrayList<VirtualFile> getAllCodecastFiles(Collection<VirtualFile> javaFiles){
+        ArrayList<VirtualFile> codecastFiles = new ArrayList<>();
+        for(VirtualFile file : javaFiles){
+            CharSequence text = LoadTextUtil.loadText(file);
+            String textString = new String(text.toString());
+
+            if(textString.contains("@codecast")){
+                codecastFiles.add(file);
+            }
+        }
+        return codecastFiles;
     }
 
     @Override
